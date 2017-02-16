0f13355dfe79be72a2539f49190d31894a757e79
cvt3894 <cvt3894@gmail.com>
2017-02-16 17:58:26 +0700

    hide is already defined

--- src/Actions.js
+++ src/Actions.js
@@ -158,7 +158,7 @@ class Actions {
       refs[el.key] = { key: el.key, name: el.key, ...el.props, type: ActionConst.REFRESH,
         base: baseKey, parent: subStateParent };
       if (this[el.key]) {
-        console.log(`Key ${el.key} is already defined!`);
+        //console.log(`Key ${el.key} is already defined!`);
       }
       this[el.key] =
         (props = {}) => {
